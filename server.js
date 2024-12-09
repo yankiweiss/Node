b8efc7b2078177b5ed4547b47ed2ba5f296461e2
yankiweiss <yakovw2706@gmail.com>
2024-12-09 10:19:16 -0500

    update

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const { error } = require('console');
-const fs = require('fs');
+const fsPromises = require('fs').promises;
 const path = require('path')
 
-fs.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf8' ,(err, data) => {
+const fileOps = async () => {
+    try {
+        const data = await fsPromises.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf8')
+        console.log(data)
+        await fsPromises.writeFile(path.join(__dirname, 'files', 'promiseWrite.txt'), data)
+        await fsPromises.appendFile(path.join(__dirname, 'files', 'promiseWrite.txt'), '\n\n\nNice To Meet you!');
+        await fsPromises.appendFile(path.join(__dirname, 'files', 'promiseWrite.txt'), '\n\n\nNice To Meet you!');
+    } catch (err) {
+        console.log(err)
+    }
+}
+/*fs.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf8' ,(err, data) => {
     if (err) throw err;
     console.log(data.toString())
 })
@@ -16,7 +27,12 @@ fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), 'Nice to Meet You', (er
     fs.appendFile(path.join(__dirname, 'files', 'reply.txt'), '\n\nYes it is!', (err) => {
         if (err) throw err;
         console.log('Append Completed!')
-    })
+
+        fs.rename(path.join(__dirname, 'files', 'reply.txt'), path.join(__dirname, 'files', 'newReply.txt'), (err) => {
+            if (err) throw err;
+            console.log('Rename Completed!')
+        })
+    })  
 })
 
  
@@ -24,4 +40,6 @@ fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), 'Nice to Meet You', (er
 process.on('uncaughtException' , err => {
     console.error(` ${err}`);
     process.exit(1)
-})
+})*/
+
+fileOps()
