ea32f6be28fba5d670043bc174d5a4ff9a1a73ef
yankiweiss <yakovw2706@gmail.com>
2024-12-09 14:58:14 -0500

    update

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-console.log('Testing!')
-console.log('yes')
+const {format } = require ('date-fns');
+const {v4: uuid} = require('uuid');
 
+console.log(format(new Date (), 'yyyyMMdd\tHH:mm:ss'))
 
+console.log(uuid())
 
 
 
