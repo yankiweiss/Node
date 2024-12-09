3c0b65a7be28efc300d65c111d31f7369e1fa924
yankiweiss <yakovw2706@gmail.com>
2024-12-09 10:10:34 -0500

    updated the server.js File

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
+const { error } = require('console');
 const fs = require('fs');
+const path = require('path')
 
-fs.readFile('./files/starter.txt', (err, data) => {
+fs.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf8' ,(err, data) => {
     if (err) throw err;
     console.log(data.toString())
+})
+
+console.log('Hello....')
+
+fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), 'Nice to Meet You', (err) => {
+    if (err) throw err;
+    console.log('Wrtie Completed!')
+
+    fs.appendFile(path.join(__dirname, 'files', 'reply.txt'), '\n\nYes it is!', (err) => {
+        if (err) throw err;
+        console.log('Append Completed!')
+    })
+})
+
+ 
+
+process.on('uncaughtException' , err => {
+    console.error(` ${err}`);
+    process.exit(1)
 })
